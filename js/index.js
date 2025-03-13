bc5616f9375c5f47d9d57f829503b207bc763f32
Dara <dara.code.creations@gmail.com>
2025-03-13 01:12:27 -0500

    Add coin flip functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 console.log("Hello world");
+
+const result = () => (Math.random() < .5) ? "Heads" : "Tails";
+
+document.querySelector(".random-button").addEventListener("click", () => {
+  const myResult = result();
+  document.querySelector("#result").textContent = myResult;
+  document.querySelector("#result-img").src = "resources/" + myResult.toLocaleLowerCase() + ".svg";
+})
